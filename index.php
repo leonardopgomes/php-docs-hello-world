e069da0479dc7473c370ff6b4baca17ff388c814
leonardopgomes <57715788+leonardopgomes@users.noreply.github.com>
2019-11-13 11:19:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-
+<html>
+<head>
+<title>Titulo</title>
+<style type="text/css">
+#text {
+   color:#COR 00;
+   font-size: 34px;
+}
+</style>
+</head>
+<body>
 <?php
 
 echo "Olá David e Daiane!"
 ?>
+  
+  </body>
+</html>
